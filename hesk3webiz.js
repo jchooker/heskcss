99a8f8f0065d2eea5c2470223583e072091fd214
Joseph Hooker <Joseph.Hooker@arkansas.gov>
2024-07-08 10:31:59 -0500

    2:changed val fxn to text

--- hesk3webiz.js
+++ hesk3webiz.js
@@ -183,7 +183,7 @@ $(document).ready(function() { //select category page section
         const fromCheck = /((\b[a-zA-Z]+\b))+/g; //and then the part without a space after it
         var matchArr = (fromField.text().trim()).split(fromCheck);
         var toOutput = matchArr.join('.') + "@arkansas.gov";
-        toField.val() = toOutput.toLowerCase();
+        toField.text(toOutput.toLowerCase());
     }
     
     });
